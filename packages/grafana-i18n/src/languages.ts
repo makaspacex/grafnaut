7e79de3f22d223fc4c6e17c16a05689ee2cae8cb
makaspacex <makaspacex@outlook.com>
2026-02-20 21:08:33 +0800

    clean

--- packages/grafana-i18n/src/languages.ts
+++ packages/grafana-i18n/src/languages.ts
@@ -33,22 +33,5 @@ interface TranslationDefinition {
  */
 export const LANGUAGES: TranslationDefinition[] = [
   { code: ENGLISH_US, name: 'English' },
-  { code: FRENCH_FRANCE, name: 'Français' },
-  { code: SPANISH_SPAIN, name: 'Español' },
-  { code: GERMAN_GERMANY, name: 'Deutsch' },
   { code: CHINESE_SIMPLIFIED, name: '中文（简体）' },
-  { code: BRAZILIAN_PORTUGUESE, name: 'Português Brasileiro' },
-  { code: CHINESE_TRADITIONAL, name: '中文（繁體）' },
-  { code: ITALIAN_ITALY, name: 'Italiano' },
-  { code: JAPANESE_JAPAN, name: '日本語' },
-  { code: INDONESIAN_INDONESIA, name: 'Bahasa Indonesia' },
-  { code: KOREAN_KOREA, name: '한국어' },
-  { code: RUSSIAN_RUSSIA, name: 'Русский' },
-  { code: CZECH_CZECHIA, name: 'Čeština' },
-  { code: DUTCH_NETHERLANDS, name: 'Nederlands' },
-  { code: HUNGARIAN_HUNGARY, name: 'Magyar' },
-  { code: PORTUGUESE_PORTUGAL, name: 'Português' },
-  { code: POLISH_POLAND, name: 'Polski' },
-  { code: SWEDISH_SWEDEN, name: 'Svenska' },
-  { code: TURKISH_TURKEY, name: 'Türkçe' },
 ];
